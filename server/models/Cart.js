304fa3e2e894e89a884ee99eae8366907c8d9c6e
himanshutank <himanshutank1111@gmail.com>
2017-12-04 16:32:04 +0530

    no msg

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -5,8 +5,8 @@ const cartSchema = new Schema({
     //service_id: {type: String, require: true},
     //user_id: {type: String, require: true},
     price: {type: Number, required: true},
-    services: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Service'}],
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    services: [{ type: mongoose.Schema.Types.ObjectId, ref: 'service'}],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user'},
 }, {
     timestamp: true
 });
