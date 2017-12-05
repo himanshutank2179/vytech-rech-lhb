76fe11a408d72d3ec9574fe20573b1ec8d1046e0
himanshutank <himanshutank1111@gmail.com>
2017-12-05 11:40:52 +0530

    no msg

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    order_id: {type: String},
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'user'},
     status: {type: Number, default: 1}, // 1 = pending, 2 = order placed
 }, {
