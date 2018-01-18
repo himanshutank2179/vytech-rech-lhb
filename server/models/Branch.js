e7ed86880c3d595f6ccf2285902149e3dd0f077b
himanshutank <himanshutank1111@gmail.com>
2018-01-18 19:15:06 +0530

    no msg

--- server/models/Branch.js
+++ server/models/Branch.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 
 const branchSchema = new Schema({
         name: {type: String, require: true}
+        address: {type: String, require: true}
+        phone: {type: String, require: true}
+        pincode: {type: String, require: true}
+
     },
     {
         timestamp: true
